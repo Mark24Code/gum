3e3ec4f4a37ab1e4d952fe80884b1852b9857f58
Mark24 <mark.zhangyoung@qq.com>
2016-07-14 12:50:12 +0800

    添加 error.html

--- controllers/example.js
+++ controllers/example.js
@@ -1,4 +1,6 @@
 
 exports.index = function(req,res,next){
+    res.status(200);
     res.render('example',{'example':'This is example'});
-}
+};
+
--- controllers/site.js
+++ controllers/site.js
@@ -1,4 +1,5 @@
 
 exports.index = function(req,res,next){
+    res.status(200);
     res.render('index',{'index':'前端测试传参'});
-}
+};
