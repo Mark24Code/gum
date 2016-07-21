2f88af3af247aaca8efd63564ba97eb761efe421
Mark24 <mark.zhangyoung@qq.com>
2016-07-21 15:57:32 +0800

    fix

--- controllers/transfer.js
+++ controllers/transfer.js
@@ -1,6 +1,6 @@
 
 exports.index = function(req,res,next){
     res.status(200);
-    res.render('transfer',{'title':'This is transfer example'});
+    res.render('transfer',{'title':'通信','transfer':'This is transfer example'});
 };
 
