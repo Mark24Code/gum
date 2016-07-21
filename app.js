b13a0ddf43bcdf8e3125c0c7530955e495d978fd
Mark24 <mark.zhangyoung@qq.com>
2016-07-21 15:32:39 +0800

    增加了注释

--- app.js
+++ app.js
@@ -10,19 +10,23 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var swig = require('swig');
+var ejs = require('ejs');
 
-/* Router.路由 */
+/* Router.路由&接口导入 */
 var webRouter = require('./web_router');
 var apiRouterV1 = require('./api_router_v1');
 
 /* App.应用实例 */
 var app = express();
 
-// view engine setup
+/* view engine setup.模板引擎 */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
+// app.engine('html',ejs.renderFile);
 app.engine('html',swig.renderFile);
 
+
+/* Middleware 中间件 */
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -31,21 +35,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/static',express.static(path.join(__dirname, 'public')));
 
-//router
+/* Router.路由&接口 */
 app.use('/api/v1',apiRouterV1);
 app.use('/', webRouter);
 
-// catch 404 and forward to error handler
+/* catch 404 and forward to error handler 404错误处理 */
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
+/* error handles 错误处理 */
+// development error handler will print stacktrace  开发错误处理，将会打印堆栈
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -56,8 +58,8 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+/* production error handler 生产环境错误处理*/
+// no stacktraces leaked to user 不会暴露错误栈给用户
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
