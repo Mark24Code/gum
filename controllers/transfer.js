6183674c332ef0492ed1b3034631069b17c3d541
Mark24 <mark.zhangyoung@qq.com>
2016-07-22 14:10:15 +0800

    添加message

--- controllers/transfer.js
+++ controllers/transfer.js
@@ -1,6 +1,21 @@
 
 exports.index = function(req,res,next){
     res.status(200);
-    res.render('transfer',{'title':'通信','transfer':'This is transfer example'});
+    res.render('./message/parent',{'title':'通信','transfer':'This is transfer example'});
+};
+
+exports.iframe1 = function(req,res,next){
+    res.status(200);
+    res.render('message/iframe1',{'title':'通信','transfer':'This is transfer example'});
+};
+
+exports.iframe2 = function(req,res,next){
+    res.status(200);
+    res.render('message/iframe2',{'title':'通信','transfer':'This is transfer example'});
+};
+
+exports.child = function(req,res,next){
+    res.status(200);
+    res.render('message/child',{'title':'通信','transfer':'This is transfer example'});
 };
 
--- web_router.js
+++ web_router.js
@@ -9,7 +9,6 @@ var express = require('express');
 var site = require('./controllers/site');
 var sortable = require('./controllers/sortable');
 var transfer = require('./controllers/transfer');
-var inner = require('./controllers/inner');
 
 var router = express.Router();
 
@@ -21,9 +20,10 @@ router.get('/',site.index);
 //拖拽
 router.get('/sortable',sortable.index);
 //通信
-router.get('/transfer',transfer.index);
-//iframe通信子页
-router.get('/inner',inner.index);
+router.get('/transfer/parent',transfer.index);
+router.get('/transfer/iframe1',transfer.iframe1);
+router.get('/transfer/iframe2',transfer.iframe2);
+router.get('/transfer/child',transfer.child);
 
 
 module.exports = router;
